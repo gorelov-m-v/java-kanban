280887e60dae0440e4efaf87a52df10f2d517f03
gorelov-m-v <mixer880@yandex.ru>
2023-08-09 23:59:31 +0300

    feat - написал KV-клиент

---
+++ src/main/java/http/KVTaskClient.java
@@ -0,0 +1,88 @@
+package http;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.http.client.utils.URIBuilder;
+
+public class KVTaskClient {
+
+    private String API_TOKEN;
+    private String url = "localhost:8079";
+
+    private void register() {
+        try {
+            URI uri = new URIBuilder(url + "/register")
+                    .build();
+
+            HttpRequest httpRequest = HttpRequest.newBuilder()
+                    .uri(uri)
+                    .GET()
+                    .header("Content-Type", "application/json")
+                    .version(HttpClient.Version.HTTP_1_1)
+                    .build();
+
+            HttpClient httpClient = HttpClient.newHttpClient();
+            HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+            API_TOKEN = response.body();
+        } catch (IOException | InterruptedException | URISyntaxException e) {
+            System.out.println("Во время выполнения запроса возникла ошибка.\n" +
+                    "Проверьте, пожалуйста, адрес и повторите попытку.");
+        }
+    }
+
+    public void save(String key, String value){
+        try {
+            URI uri = new URIBuilder(url + "/save/" + key)
+                    .addParameter("API_TOKEN", API_TOKEN)
+                    .build();
+
+            HttpRequest httpRequest = HttpRequest.newBuilder()
+                    .uri(uri)
+                    .POST(HttpRequest.BodyPublishers.ofString(value))
+                    .header("Content-Type", "application/json")
+                    .version(HttpClient.Version.HTTP_1_1)
+                    .build();
+
+            HttpClient httpClient = HttpClient.newHttpClient();
+            HttpResponse<String> response = httpClient.send(httpRequest,
+                    HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
+
+        } catch (IOException | InterruptedException | URISyntaxException e) {
+            System.out.println("Во время выполнения запроса возникла ошибка.\n" +
+                    "Проверьте, пожалуйста, адрес и повторите попытку.");
+        }
+    }
+
+    public String load(String key) {
+        String value;
+        try {
+            URI uri = new URIBuilder(url + "/save/" + key)
+                    .addParameter("API_TOKEN", API_TOKEN)
+                    .build();
+
+            HttpRequest httpRequest = HttpRequest.newBuilder()
+                    .uri(uri)
+                    .GET()
+                    .header("Content-Type", "application/json")
+                    .version(HttpClient.Version.HTTP_1_1)
+                    .build();
+
+            HttpClient httpClient = HttpClient.newHttpClient();
+            HttpResponse<String> response = httpClient.send(httpRequest,
+                    HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
+
+            value = response.body();
+        } catch (URISyntaxException | IOException | InterruptedException e) {
+            System.out.println("Во время выполнения запроса возникла ошибка.\n" +
+                    "Проверьте, пожалуйста, адрес и повторите попытку.");
+            value = "";
+        }
+        return value;
+    }
+}
