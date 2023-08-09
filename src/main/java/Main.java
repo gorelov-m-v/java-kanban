a3f64a321d0f457c9eef4a146758d815f96d9db7
gorelov-m-v <mixer880@yandex.ru>
2023-08-09 18:40:50 +0300

    feat - склонировал и дописал KV-сервер

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,12 @@
 import com.google.gson.Gson;
+import http.KVServer;
 import manager.InMemoryTaskManager;
 import model.Epic;
 import model.Subtask;
 import model.Task;
 import model.constant.TaskStatus;
 
+import java.io.IOException;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
@@ -14,37 +16,7 @@ import static java.time.temporal.ChronoUnit.MINUTES;
 
 public class Main {
 
-    public static void main(String[] args) {
-//        InMemoryTaskManager taskManager = new InMemoryTaskManager();
-//
-//        Task task1 = new Task("TestTaskTitle", "TestTaskDescription", null, 0);
-//        Task task2 = new Task("TestTaskTitle", "TestTaskDescription", Instant.now().plus(100, MINUTES), 40);
-//        Task task3 = new Task("TestTaskTitle", "TestTaskDescription", Instant.now().plus(150, MINUTES), 40);
-//        Task task4 = new Task("TestTaskTitle", "TestTaskDescription", Instant.now().plus(200, MINUTES), 40);
-//        Task task5 = new Task("NewTestTaskTitle", "NewTestTaskDescription", Instant.now().plus(700, MINUTES), 40);
-//        Epic epic1 = new Epic("TestTaskTitle", "TestTaskDescription");
-//        taskManager.createEpic(epic1);
-//        Subtask subtask1 = new Subtask(
-//                "TestSubtaskTitle", "TestSubtaskDescription", 1,
-//                Instant.now().plus(2250, MINUTES), 30);
-//        taskManager.createSubtask(subtask1, 1);
-//        taskManager.createTask(task1);
-//        taskManager.createTask(task2);
-//        taskManager.createTask(task3);
-//
-//        Gson gson = new Gson();
-//        String taskToString = gson.toJson(task1);
-//        System.out.println(taskToString);
-//        System.out.println(Instant.now().plus(100, MINUTES));
-//
-////
-////        System.out.println(taskManager.getAllTasks());
-////        System.out.println(taskManager.getPrioritizedTasks());
-
-        String path = "/tasks/subtask/epic/?id=1";
-
-        boolean isGetEpicSubtasks = Pattern.matches("^/tasks/subtask/epic/\\?id=\\d+$", path);
-
-        System.out.println(isGetEpicSubtasks);
+    public static void main(String[] args) throws IOException {
+        new KVServer().start();
     }
 }
